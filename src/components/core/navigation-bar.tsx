1deb2b23d22029b1474f0d4d3ea73cb4604e8019
Eric Zhang <superezzhang@gmail.com>
2025-01-02 00:21:34 -0500

    Add working nav bar

--- src/components/core/navigation-bar.tsx
+++ src/components/core/navigation-bar.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -11,17 +13,39 @@ import {
 
 export default function NavigationBar() {
   return (
-  <div className="bg-slate-500">
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
-  </div>
+    <div className="bg-slate-500">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink>
+                Home
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/about" legacyBehavior passHref>
+              <NavigationMenuLink>
+                About
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/projects" legacyBehavior passHref>
+              <NavigationMenuLink>
+                Projects
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/contact" legacyBehavior passHref>
+              <NavigationMenuLink>
+                Contact
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
   );
 }
