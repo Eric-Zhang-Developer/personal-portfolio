f02574ffcf93117815dd4ac807a4a6c921500811
Eric Zhang <superezzhang@gmail.com>
2025-01-01 23:33:10 -0500

    move nav bar to seperate component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,10 @@
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu";
+import NavigationBar from "@/components/ui/navigation-bar";
 
 export default function Home() {
   return (
     <div className="bg-slate-300">
       <main>
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <NavigationMenuLink>Link</NavigationMenuLink>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+        <NavigationBar></NavigationBar>
       </main>
     </div>
   );
