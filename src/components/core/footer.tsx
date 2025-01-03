423610165bd0786edc494652ec35db3bd894a83f
Eric Zhang <superezzhang@gmail.com>
2025-01-03 16:00:09 -0500

    Add github and linkedin connections to footer

--- src/components/core/footer.tsx
+++ src/components/core/footer.tsx
@@ -1,8 +1,23 @@
+import { Github, Linkedin } from "lucide-react";
 
-export default function Footer(){
+export default function Footer() {
   return (
-    <div className="fixed bottom-0 bg-slate-800 w-full h-10 border-t border-black text-white text-sm flex justify-center items-center">
-      Copyright © 2025 Eric Zhang
+    <div className="fixed bottom-0 bg-slate-800 w-full h-10 border-t border-black flex justify-center items-center">
+      <p className="text-white text-sm mr-4">Copyright © 2025 Eric Zhang</p>
+      <a
+        href="https://github.com/Eric-Zhang-Developer"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Github size={20} color="white" className="mr-4"/>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/eric-zhang-developer/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Linkedin size={20} color="white"/>
+      </a>
     </div>
-  )
+  );
 }
