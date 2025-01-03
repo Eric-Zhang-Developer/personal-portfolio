6574dcf6f60a10dc3fc038e19874bca5fe208c25
Eric Zhang <superezzhang@gmail.com>
2025-01-02 20:51:06 -0500

    Add contact button and projects button placeholders to hero section

--- src/components/core/hero-section.tsx
+++ src/components/core/hero-section.tsx
@@ -3,6 +3,11 @@ export default function HeroSection() {
     <div className="flex flex-col justify-center items-center">
       <h1 className="text-slate-800 text-4xl">Hi, I'm Eric Zhang</h1>
 
+      <div>
+        <button className="bg-slate-800 text-white p-2">Contact Me</button>
+        <button className="border border-slate-800 p-2 shadow-inner">Projects</button>
+      </div>
+
     </div>
   );
 }
