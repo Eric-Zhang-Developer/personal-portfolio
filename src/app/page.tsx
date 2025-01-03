5f0da9d6af19ec8d70e5a5aa650358e317fa4fab
Eric Zhang <superezzhang@gmail.com>
2025-01-02 20:50:17 -0500

    Format hero title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@ import HeroSection from "@/components/core/hero-section";
 
 export default function Home() {
   return (
-    <div className="bg-slate-300">
-      <main>
-        <HeroSection></HeroSection>
-      </main>
-    </div>
+    <main>
+      <HeroSection></HeroSection>
+    </main>
   );
 }
--- src/components/core/hero-section.tsx
+++ src/components/core/hero-section.tsx
@@ -1,7 +1,11 @@
 export default function HeroSection() {
   return (
-    <div>
-      Hi, I'm Eric Zhang
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-slate-800 text-4xl">Hi, I'm Eric Zhang</h1>
+      <div>
+        <button className="bg-slate-800 text-white p-2">Contact Me</button>
+        <button className="border border-slate-800 p-2 shadow-inner">Projects</button>
+      </div>
     </div>
   );
 }
