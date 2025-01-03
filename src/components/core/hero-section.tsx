e15301bcf2b1539f17296ac83f1d265023f96e73
Eric Zhang <superezzhang@gmail.com>
2025-01-02 20:50:26 -0500

    Formate hero title

--- src/components/core/hero-section.tsx
+++ src/components/core/hero-section.tsx
@@ -2,10 +2,7 @@ export default function HeroSection() {
   return (
     <div className="flex flex-col justify-center items-center">
       <h1 className="text-slate-800 text-4xl">Hi, I'm Eric Zhang</h1>
-      <div>
-        <button className="bg-slate-800 text-white p-2">Contact Me</button>
-        <button className="border border-slate-800 p-2 shadow-inner">Projects</button>
-      </div>
+
     </div>
   );
 }
