24400cbc7ef63ca0c0f1be4a6e617db414fe8eaa
Eric Zhang <superezzhang@gmail.com>
2025-01-03 16:21:21 -0500

    Add projects grid, project placeholders

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,17 @@
+
+
 export default function Projects() {
   return (
-    <div>
-      <main>Projects</main>
-    </div>
+    <main className="flex flex-col container mx-auto px-4 justify-center items-center">
+      <h1 className="text-slate-800 text-4xl mt-6">My Projects</h1>
+      <section className="grid gap-2 mt-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+        <p>Project 1</p>
+        <p>Project 2</p>
+        <p>Project 3</p>
+        <p>Project 4</p>
+
+      </section>
+      
+    </main>
   );
 }
