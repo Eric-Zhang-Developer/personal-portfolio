65398e8aac8a92319707741b2be69aabd385846d
Eric Zhang <superezzhang@gmail.com>
2025-01-02 20:28:32 -0500

    Add proper footer with copyright notice

--- src/components/core/footer.tsx
+++ src/components/core/footer.tsx
@@ -1,8 +1,8 @@
 
 export default function Footer(){
   return (
-    <div className="fixed bottom-0 bg-slate-500 w-full h-10 border-t border-black">
-      Footer
+    <div className="fixed bottom-0 bg-slate-800 w-full h-10 border-t border-black text-white text-sm flex justify-center items-center">
+      Copyright © 2025 Eric Zhang
     </div>
   )
 }
