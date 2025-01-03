3c6c26ccadb2034f340c964b0cd4fa6f4170bc7a
Eric Zhang <superezzhang@gmail.com>
2025-01-02 21:38:55 -0500

    Add proper functionality to hero section buttons, also add styling

--- src/components/core/hero-section.tsx
+++ src/components/core/hero-section.tsx
@@ -1,14 +1,26 @@
+import Link from "next/link";
+
 export default function HeroSection() {
   return (
     <div className="flex flex-col justify-center items-center gap-4 mt-4">
       <h1 className="text-slate-800 text-4xl">Hi, I'm Eric Zhang</h1>
-      <p className="text-slate-800 text-xl">Forging robust web applications with precision and purpose</p>
+      <p className="text-slate-700 text-xl">
+        Forging robust web applications with precision and purpose
+      </p>
 
       <div>
-        <button className="bg-slate-800 text-white p-2">Contact Me</button>
-        <button className="border border-slate-800 p-2 shadow-inner">
-          Projects
-        </button>
+        <Link
+          href="/projects"
+          className="transform bg-slate-800 text-white py-2 px-4 mr-4 rounded-lg shadow-md transition  hover:scale-110 hover:shadow-xl"
+        >
+          View Projects
+        </Link>
+        <Link
+          href="/contact"
+          className="transform border-2 text-slate-800 border-slate-800 py-2 px-4 rounded-lg shadow-md transition hover:scale-105 hover:shadow-xl"
+        >
+          Contact Me
+        </Link>
       </div>
     </div>
   );
