dcb14cabff4d08fbe8a878e8f08c93481279db01
Eric Zhang <superezzhang@gmail.com>
2025-01-04 21:28:32 -0500

    Add github and linkedin contacts to contact form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,26 @@
 import ContactForm from "@/components/core/contact-form";
+import { Github, Linkedin } from "lucide-react";
 
 export default function Contact() {
   return (
-    <main>
+    <main className="flex flex-col items-center gap-6">
       <ContactForm></ContactForm>
+      <section className="flex flex-row text-primary">
+        <a
+          href="https://github.com/Eric-Zhang-Developer"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Github size={40} className="mr-6"/>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/eric-zhang-developer/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Linkedin size={40} />
+        </a>
+      </section>
     </main>
 
   );
