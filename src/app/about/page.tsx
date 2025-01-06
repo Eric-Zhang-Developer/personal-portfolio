21a68fdacb2b0422ba497ee3972c3d3c8ac5d62b
Eric Zhang <superezzhang@gmail.com>
2025-01-05 23:25:46 -0500

    Add wireframe for first section of the about me page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,22 @@
 export default function About() {
   return (
-    <div>
-      <main>About</main>
-    </div>
+    <>
+      <section className="flex flex-col container mx-auto items-center text-primary">
+        <h1 className="text-4xl mt-6">About Me</h1>
+        <p className="mt-2 text-secondary text-lg text-center p-4 lg:p-2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident
+        </p>
+        <div className="flex items-center justify-center flex-col mt-6">
+          <span className="text-accent">Scroll</span>
+          <div className="h-32 w-px bg-gradient-to-b from-accent
+          to-transparent animate-bounce mt-8"></div>
+        </div>
+      </section>
+    </>
   );
 }
