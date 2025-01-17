c4c6e48f619d40565fe0ca29ff2e171d4a3815b0
Eric Zhang <superezzhang@gmail.com>
2025-01-16 23:07:37 -0500

    Remove contact me button from hero section, make typography larger

--- src/components/core/hero-section.tsx
+++ src/components/core/hero-section.tsx
@@ -2,26 +2,19 @@ import Link from "next/link";
 import Image from "next/image";
 export default function HeroSection() {
   return (
-    <div className="flex flex-col justify-center items-center gap-6 mt-6 p-1">
-      <h1 className="text-primary text-5xl font-bold text-center">Hello, I&apos;m <span className="text-accent">Eric Zhang</span></h1>
-      <p className="text-secondary text-2xl text-center mb-8">
+    <div className="flex flex-col justify-center items-center gap-6 p-1">
+      <h1 className="text-primary text-6xl font-bold text-center">Hello, I&apos;m <span className="text-accent">Eric Zhang</span></h1>
+      <p className="text-secondary text-3xl text-center mb-8">
         Forging robust web applications with precision and purpose
       </p>
 
       <div className="mb-8">
         <Link
           href="/projects"
-          className="transform bg-secondary text-white text-xl 
-          py-3 px-5 mr-4 rounded-lg shadow-md transition hover:shadow-xl hover:bg-accent"
-        >
-          View Projects
-        </Link>
-        <Link
-          href="/contact"
-          className="transform border-2 text-primary border-primary text-xl py-3 px-5
+          className="transform border-2 text-primary border-primary text-xl py-4 px-8
           rounded-lg shadow-md transition hover:border-accent hover:text-accent hover:shadow-xl"
         >
-          Contact Me
+          View Projects
         </Link>
       </div>
 
