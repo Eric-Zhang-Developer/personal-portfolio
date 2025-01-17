f9a6fd4817e3f020b2ca5175839ad862129cc369
Eric Zhang <superezzhang@gmail.com>
2025-01-16 23:18:14 -0500

    Format Hero section to sit in the middle of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from "@/components/core/hero-section";
 
 export default function Home() {
   return (
-    <section>
+    <section className=" flex flex-1 min-h-[calc(100vh-theme(spacing.16)-theme(spacing.14))] justify-center items-center">
       <HeroSection></HeroSection>
     </section>
   );
