4fd3af231ffbd82168e9e9533021fdf4d350bc9f
Eric Zhang <superezzhang@gmail.com>
2025-01-16 22:43:56 -0500

    Make contact page not deadly to look at

--- src/components/core/contact-form.tsx
+++ src/components/core/contact-form.tsx
@@ -26,7 +26,7 @@ export default function ContactForm() {
         id="email"
         type="email"
         name="email"
-        className="w-full h-8 mb-2 shadow-lg rounded-md p-2"
+        className="w-full h-8 mb-2 shadow-md rounded-md p-2 bg-slate-800 shadow-accent/20"
       />
       <ValidationError prefix="Email" field="email" errors={state.errors} />
       <label htmlFor="email" className="text-xl self-start ">
@@ -35,14 +35,14 @@ export default function ContactForm() {
       <textarea
         id="message"
         name="message"
-        className="w-full h-96 shadow-lg rounded-md p-2"
+        className="w-full h-96 shadow-md shadow-accent/20 rounded-md p-2 bg-slate-800 s"
       />
       <ValidationError prefix="Message" field="message" errors={state.errors} />
       <button
         type="submit"
         disabled={state.submitting}
-        className="transform bg-primary text-white text-2xl mt-6
-          py-3 px-4 mr-4 rounded-lg shadow-md flex justify-center items-center gap-2
+        className="transform bg-slate-800 text-white text-2xl mt-6
+          py-3 px-4 mr-4 rounded-lg shadow-md shadow-accent/20 flex justify-center items-center gap-2
           transition hover:shadow-xl hover:bg-accent hover:scale-105
           w-3/4 lg:w-2/4"
       >
