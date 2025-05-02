88e119a39fbf4d9f22d3a20a34d387708b233eba
Eric Zhang <superezzhang@gmail.com>
2025-05-01 21:03:53 -0400

    reafactor project card to have main link be in a div

--- src/components/core/project-card.tsx
+++ src/components/core/project-card.tsx
@@ -12,27 +12,28 @@ type ProjectCardProps = {
 export default function ProjectCard({ project }: ProjectCardProps) {
   const { title, description, imagePath, skills, link } = project;
   return (
-    <Link
-      href={link}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="flex flex-col items-center border-4 p-4 hover:cursor-pointer hover:shadow-2xl
-    gap-2 border-secondary rounded-2xl transition hover:border-accent hover:-translate-y-1 shadow-lg shadow-accent/30
+    <article>
+      <div
+        className="border-4 p-4  hover:shadow-2xl h-full
+     border-secondary rounded-2xl transition hover:border-accent hover:-translate-y-1 shadow-lg shadow-accent/30
     bg-gradient-to-br from-blue-900/40 to-slate-800/50"
-    >
-      <Image src={imagePath} alt={title} width={100} height={100}></Image>
-      <h1 className="text-primary text-3xl ">{title}</h1>
-      <p className="text-secondary text-lg text-center">{description}</p>
-      <section className="flex flex-row flex-wrap justify-center">
-        {skills.map((skill) => (
-          <span
-            key={skill}
-            className="px-3 py-1 text-s text-border bg-slate-100 rounded-full m-1 inline-block border-l-8 border-2 border-accent"
-          >
-            {skill}
-          </span>
-        ))}
-      </section>
-    </Link>
+      >
+        <Link href={link} target="_blank" rel="noopener noreferrer" className="flex flex-col items-center gap-2 hover:cursor-pointer">
+          <Image src={imagePath} alt={title} width={100} height={100}></Image>
+          <h1 className="text-primary text-3xl ">{title}</h1>
+          <p className="text-secondary text-lg text-center">{description}</p>
+          <section className="flex flex-row flex-wrap justify-center">
+            {skills.map((skill) => (
+              <span
+                key={skill}
+                className="px-3 py-1 text-s text-border bg-slate-100 rounded-full m-1 inline-block border-l-8 border-2 border-accent"
+              >
+                {skill}
+              </span>
+            ))}
+          </section>
+        </Link>
+      </div>
+    </article>
   );
 }
