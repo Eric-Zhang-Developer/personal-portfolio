beb0897923793e30cadd294ce0568ba456ad13ba
Eric Zhang <superezzhang@gmail.com>
2025-05-27 14:12:04 -0400

    change items to slide in from bottom

--- src/components/core/hero-section.tsx
+++ src/components/core/hero-section.tsx
@@ -7,16 +7,16 @@ const containerVariants: Variants = {
   hidden: {},
   visible: {
     transition: {
-      staggerChildren: 0.5,
+      staggerChildren: 0.4,
     },
   },
 };
 
 const itemVariants: Variants = {
-  hidden: { opacity: 0, x: 50 },
+  hidden: { opacity: 0, y: 50 },
   visible: {
     opacity: 1,
-    x: 0,
+    y: 0,
     transition: {
       duration: 0.4,
       ease: "easeOut",
